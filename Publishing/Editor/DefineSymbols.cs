4fba3e12a4242fd6396a487950818e0f19dfe95f
devnem0y <devnem0y@gmail.com>
2024-10-24 11:18:38 +0500

    [Advertisement] Mediation switcher

---
+++ Publishing/Editor/DefineSymbols.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using UnityEditor;
+
+namespace Dreamsim.Publishing.Editor
+{
+    public static class DefineSymbols
+    {
+        public static void Add(string scriptingDefine)
+        {
+            var buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+            var defineSymbolsForGroup = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
+            
+            if (defineSymbolsForGroup.Contains(scriptingDefine)) return;
+            
+            var defines = defineSymbolsForGroup + ";" + scriptingDefine;
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, defines);
+        }
+
+        public static void Remove(string scriptingDefine)
+        {
+            var buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+            var defineSymbolsForGroup = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
+            var list = defineSymbolsForGroup.Split(';').ToList();
+            
+            if (!list.Contains(scriptingDefine)) return;
+            
+            list.Remove(scriptingDefine);
+            var defines = string.Join(";", list);
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, defines);
+        }
+    }
+}
