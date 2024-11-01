6b3bf2b9f0ef8c299bded758be040062edb3f2d9
devnem0y <devnem0y@gmail.com>
2024-11-01 17:34:35 +0500

    [Tech] Firebase initialization

---
+++ Assets/DreamsimPackageRoot/Publishing/Runtime/Analytics/FirebaseManager.cs
@@ -0,0 +1,30 @@
+﻿using Cysharp.Threading.Tasks;
+using Firebase;
+using UnityEngine;
+
+namespace Dreamsim.Publishing
+{
+    public class FirebaseManager : MonoBehaviour
+    {
+        private FirebaseApp _app;
+        
+        public FirebaseApp App => _app;
+        
+        internal async UniTask InitAsync()
+        {
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                var status = await FirebaseApp.CheckAndFixDependenciesAsync();
+                
+                if (status == DependencyStatus.Available) Init();
+                else Debug.LogError("Could not resolve all Firebase dependencies: " + status);
+            }
+            else Init();
+        }
+
+        private void Init()
+        {
+            _app = FirebaseApp.DefaultInstance;
+        }
+    }
+}
